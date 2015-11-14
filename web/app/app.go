c569bb7344dd84dcc094d5a4a5e14f859c29dc45
Y Okumura <uokumura@me.com>
2015-11-14 20:38:19 +0900

    JSONレスポンスを作成

--- web/app/app.go
+++ web/app/app.go
@@ -1,14 +1,28 @@
 package picture_counter
 
 import (
+    "encoding/json"
     "fmt"
     "net/http"
 )
 
 func init() {
-    http.HandleFunc("/hello", handler)
+    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/count", count)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello, " + r.FormValue("name"))
+}
+
+func count(w http.ResponseWriter, r *http.Request) {
+    marks := [...]Mark { Mark{123, 456, 15}, Mark{321, 654, 11} }
+    data, _ := json.Marshal(marks)
+    fmt.Fprint(w, string(data))
+}
+
+type Mark struct {
+    X int `json:"x"`
+    Y int `json:"y"`
+    Size int `json:"size"`
 }
